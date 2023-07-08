bb74a302f66b47d83bf542f4476100252415857c
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-07-07 23:43:01 -0400

    Added Update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // DEPENDENCIES
 const express = require("express");
+const cors = require("cors");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
 app.use(express.json()); // Parse incoming JSON
+app.use(cors());
 
 // ROUTES
 app.get("/", (req, res) => {
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,9 +2,15 @@ const express = require("express");
 const logs = express.Router();
 const logsArray = require("../models/log");
 
+// INDEX
 logs.get("/", (req, res) => {
     res.json(logsArray);
 });
+
+// logs.get ("/", (req, res) => {
+//     const foo = JSON.parse(JSON.stringify(logsArray)) ;
+//     res.status(200).json(foo);
+// });
 
 // SHOW
 logs.get("/:arrayIndex", (req, res) => {
@@ -12,7 +18,7 @@ logs.get("/:arrayIndex", (req, res) => {
         res.json(logsArray[req.params.arrayIndex]);
     }
     else {
-        res.status(404).redirect('/404');
+        res.status(404).redirect("/404");
     }
 });
 
@@ -29,8 +35,19 @@ logs.delete("/:arrayIndex", (req, res) => {
         res.json(deletedLog);
     }
     else {
-        res.status(404).redirect(" /404");
+        res.status(404).redirect("/404");
     }
 });
+
+// UPDATE
+logs.put("/:arrayIndex", (req, res) => {
+    if (logsArray[req.params.arrayIndex]) {
+        logsArray[req.params.arrayIndex] = req.body;
+        res.status(200).json(logsArray[req.params.arrayIndex]);
+    }
+    else {
+        res.status(404).redirect("/404");
+    }
+  });
 
 module.exports = logs;
